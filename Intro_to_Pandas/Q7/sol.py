b642cae7a267c0052079f055c1e574b7a9566360
ganeshkumarka <ganeshkumarka03@gmail.com>
2024-10-02 13:11:29 +0530

    qp 7 completed

---
+++ Intro_to_Pandas/Q7/sol.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    valid_rows = []
+
+    for index, row in students.iterrows():
+        if pd.notna(row['name']):
+            valid_rows.append(row)
+    result = pd.DataFrame(valid_rows)
+    
+    return result
+
+
+data = {'student_id': [32, 217, 779, 849],
+        'name': ['Piper', None, 'Georgia', 'Willow'],
+        'age': [5, 19, 20, 14]}
+
+students = pd.DataFrame(data)
+
+
+result = dropMissingData(students)
+print(result)
