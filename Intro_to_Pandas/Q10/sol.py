4d857ac96391a1cc9463426b41b690408bdfd065
ganeshkumarka <ganeshkumarka03@gmail.com>
2024-10-03 13:06:12 +0530

    task 10 completed

--- Intro_to_Pandas/Q10/sol.py
+++ Intro_to_Pandas/Q10/sol.py
@@ -3,3 +3,8 @@ import pandas as pd
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
     students['grade'] = students['grade'].astype(int)
     return students
+
+data = {'student_id': [32, 217, 779, 849],
+        'name': ['Piper', 'Georgia', 'Willow', 'Henry'],
+        'grade': ['A', 'B', 'C', 'D']}
+students = pd.DataFrame(data)
